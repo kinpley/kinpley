83041359e3d3d6f49d14d331c5487d65fd9c11f4
kinpley <plaids-snowmen-4h@icloud.com>
2025-11-22 17:18:51 +0100

    Add files via upload

---
+++ start-mining-ironfish-and-tari-sha3x.bat
@@ -0,0 +1,16 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+:: Start miner with administrator privileges to enable gpu-tweak-profile
+
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_MAX_USE_SYNC_OBJECTS 1
+setx GPU_SINGLE_ALLOC_PERCENT 100
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_MAX_SINGLE_ALLOC_PERCENT 100
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --algorithm fishhash --pool de.ironfish.herominers.com:1145 --wallet ironfish-wallet --algorithm sha3x --pool tari.luckypool.io:6118 --wallet tari-wallet
+pause
